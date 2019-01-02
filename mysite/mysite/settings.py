65ca2b1ad9919014f8add9b63ffcda5ae20e4022
zhangshuxian <zhangsc678@icloud.com>
2019-01-02 16:45:57 +0800

    clear dairy migrations

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'dairy.apps.DairyConfig',
+    'diary.apps.DiaryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('diary/', include('dairy.urls')),
+    path('diary/', include('diary.urls')),
     path('admin/', admin.site.urls),
 ]
