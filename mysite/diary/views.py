690efc1e5f82c27b8e69086c3ff4e47993ece9ea
zhangshuxian <zhangsc678@icloud.com>
2019-01-18 16:25:33 +0800

    add earlier_than, later_than, order_by query param

--- mysite/diary/views.py
+++ mysite/diary/views.py
@@ -11,36 +11,52 @@ from rest_framework.decorators import api_view
 from .serializers import DiarySerializer
 from .models import Diary
 import logging
+from datetime import datetime
 from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
 
 logger = logging.getLogger('dairy.users.views')
 
 @api_view(['GET', 'POST'])
 @login_required
 def diary_list(request):
+    """
+        get diary list
+        /diaries/?page=&page_size=&year=&month&search=&earlier_than=&later_than=&orderBy=
+    """
     # logger.debug(repr(DiarySerializer()))
     user_id = request.user.id
     if request.method == 'GET':
-        diaries = Diary.objects.all().filter(author=user_id).order_by('-datetime')
+        diaries = Diary.objects.all().filter(author=user_id)
         if request.GET.get('year', '') != '':
             diaries = diaries.filter(year=request.GET.get('year'))
         if request.GET.get('month', '') != '':
-            logger.debug('month={}'.format(request.GET.get('month')))
             diaries = diaries.filter(month=request.GET.get('month'))
         if request.GET.get('search', '') != '':
             diaries = diaries.filter(content__icontains=request.GET.get('search'))
+        if request.GET.get('earlier_than', '') != '':
+            dt = datetime.strptime(request.GET.get('earlier_than'), settings.REST_FRAMEWORK.get('DATETIME_FORMAT'))
+            diaries = diaries.filter(datetime__lt=dt)
+        if request.GET.get('later_than', '') != '':
+            # logger.debug('later_than={}'.format(request.GET.get('later_than')))
+            dt = datetime.strptime(request.GET.get('later_than'), settings.REST_FRAMEWORK.get('DATETIME_FORMAT'))
+            # logger.debug('conver to time: {}'.format(dt))
+            diaries = diaries.filter(datetime__gt=dt)
+        order = request.GET.get('order_by', '-datetime')
+        diaries = diaries.order_by(order)
         page = request.GET.get('page')
         if page is None:
             serializer = DiarySerializer(diaries, many=True)
+            return JsonResponse({'data': serializer.data}, safe=False)
         else:
             years = Diary.objects.all().values('year').distinct()
             paginator = Paginator(diaries, request.GET.get('page_size', 10))
             serializer = DiarySerializer(paginator.page(page), many=True)
-        return JsonResponse({
-            'data': serializer.data,
-            'num_pages': paginator.num_pages,
-            'years': list(years)
-            }, safe=False)
+            return JsonResponse({
+                'data': serializer.data,
+                'num_pages': paginator.num_pages,
+                'years': list(years)
+                }, safe=False)
 
     elif request.method == 'POST':
         data = request.data
